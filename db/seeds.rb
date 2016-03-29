82baa41f41040e1ff25098d0ac4d2bb0a65f99ba
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-29 15:54:14 -0500

    DB Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,10 @@ ui = Chatroom.create name: "UI Design"
 mobile = Chatroom.create name: "Mobile Development"
 
 # Seed texts
+100.times do
 Text.create chatroom: ui, content: "What is the essential of the UI design?", user: tuco, email:"tuco@info.com"
 Text.create chatroom: ui, content: "Here is the list of forums and discussions for iOS developers", user: kelly, email: "kelly@info.com"
 Text.create chatroom: mobile, content: "Mobile IOS 2016", user: ivanka, email: "ivanka@info.com"
 Text.create chatroom: mobile, content: "Mobile Testing", user: tuco, email: "tuco@info.com"
 Text.create chatroom: mobile, content: "Why does my phone collect location data", user: bonya, email: "bonya@info.com"
+end
